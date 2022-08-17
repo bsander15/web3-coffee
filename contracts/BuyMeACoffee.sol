b639d04dbb29b11c96e8ba8c28d45000e12160f3
bsander15 <bsander1515@gmail.com>
2022-08-16 23:07:42 -0500

    change withdrawl address

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+
 //Deployed to Goerli: 0xb78f9EfeF62E9A32680c9f736ee2EEe871b2fb2B
 
 // Import this file to use console.log
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BuyMeACoffee {
+contract BuyMeACoffee is Ownable {
 
     //Event emmitted when Memo is created
     event NewMemo(
@@ -27,12 +29,12 @@ contract BuyMeACoffee {
     //List of memos
     Memo[] memos;
 
-    //Addres of contract deployer
-    address payable owner;
+    //Address of withdraw address
+    address payable withdraw;
 
     //Deploy logic
     constructor() {
-        owner = payable(msg.sender);
+        withdraw = payable(msg.sender);
     }
 
     /**
@@ -59,9 +61,7 @@ contract BuyMeACoffee {
     * @dev send balance of contract to owner
     */
     function withdrawTips() public {
-        owner.transfer(address(this).balance);
-        
-        
+        withdraw.transfer(address(this).balance);   
     }
 
     /**
@@ -71,5 +71,20 @@ contract BuyMeACoffee {
         return memos;
     }
 
+    /**
+     * @dev change withdrawl address
+     * @param _withdraw new withdrawl address
+     */
+     function changeWithdraw(address payable _withdraw) public onlyOwner {
+        withdraw = _withdraw;
+     }
+
+    /**
+     * @dev returns amount of tips stored in contract
+     * @return tips amount of tips
+     */
+    function getTips() public view returns(uint256) {
+        return address(this).balance;
+    }
 }
 
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,172 @@
-import logo from './logo.svg';
 import './App.css';
+import abi from './utils/BuyMeACoffee.json';
+import { ethers } from 'ethers';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const contractAddress = '0xbE2Ea55D8FD3870770e72F443e6E4022c941e21C';
+  const contractABI = abi.abi;
+
+  const [currentAccount, setCurrentAccount] = useState();
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [withdrawlAddr, setWithdrawlAddr] = useState('');
+
+  useEffect(() => {
+    isWalletConnected();
+  }, []);
+  
+  const contractInstance = async ethereum => {
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum, "any");
+      const signer = provider.getSigner();
+      const buyMeACoffee = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      return buyMeACoffee;
+    }
+  }
+
+  // Wallet connection logic
+  const isWalletConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      const accounts = await ethereum.request({method: 'eth_accounts'})
+      console.log("accounts: ", accounts);
+
+      if (accounts.length > 0) {
+        const account = accounts[0];
+        console.log("wallet is connected! " + account);
+      } else {
+        console.log("make sure MetaMask is connected");
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("install Metamask")
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts'
+      })
+
+      setCurrentAccount(accounts[0]);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const buyCoffee = async () => {
+    try {
+      const { ethereum } = window;
+
+      const contract = await contractInstance(ethereum);
+
+        console.log('Buying Coffee, just a minute!');
+        const txn = await contract.buyCoffee(
+          name? name: "anon",
+          message ? message: "",
+          {value: ethers.utils.parseEther('0.001')}
+        );
+
+        await txn.wait();
+
+        console.log("Coffee purchased");
+
+        setName("");
+        setMessage("");
+    } catch (e) {
+        console.log(e);
+    }
+  }
+
+  const withdrawTips = async () => {
+    try {
+      const { ethereum } = window;
+
+      const contract = await contractInstance(ethereum);
+
+        console.log('Withdrawing tips...');
+        const txn = await contract.withdrawTips();
+
+        await txn.wait();
+
+        console.log('Tips Withdrawn');
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const changeWithdrawl = async e => {
+    e.preventDefault();
+    try {
+      const {ethereum} = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum, "any");
+        const signer = provider.getSigner();
+        console.log(signer);
+        const buyMeACoffee = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        console.log(`Changing address to ${withdrawlAddr}`);
+        const txn = await buyMeACoffee.changeWithdraw(withdrawlAddr);
+        await txn.wait();
+        console.log(`Withdrawl address changed to ${withdrawlAddr}`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const viewTips = async () => {
+    try {
+      const {ethereum} = window;
+
+      const contract = await contractInstance(ethereum);
+      const balance = await contract.getTips();
+      console.log(`Contract balance: ${balance}`)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type='button' onClick={buyCoffee} >
+        Buy Coffee
+      </button>
+      <button type='button' onClick={connectWallet} >
+        Connect Wallet
+      </button>
+      <button type='button' onClick={withdrawTips}>
+        Withdraw Tips
+      </button>
+      <form onSubmit={changeWithdrawl} id='form1'>
+        <input type='text' value={withdrawlAddr} onChange={e => setWithdrawlAddr(e.target.value)} />
+        <button type='submit' form='form1'>
+          Change Address
+        </button>
+      </form>
+      <button type='button' onClick={viewTips}>
+        View Tips
+      </button>
     </div>
   );
 }
